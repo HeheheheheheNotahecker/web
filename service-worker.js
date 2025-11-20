960e70b16d8f74f2bd6571387b3d65797b58fd3d
HeheheheheheNotahecker <carsonryan2112@gmail.com>
2025-11-20 15:18:29 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'stealth-v1';
+const PROXY_URLS = [
+    '/',
+    '/about-blank-loader.html'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(PROXY_URLS);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
